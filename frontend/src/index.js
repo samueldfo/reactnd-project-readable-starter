b000df8660b57acd07224a5d6bcc4b62e6520bf9
Samuel Oliveira <samueldfo@gmail.com>
2018-12-05 02:33:33 -0200

    added logger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './categories/reducers'
 import { Provider } from 'react-redux'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const loggerMiddleware = createLogger()
+
 const store = createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware()
+    applyMiddleware(
+      loggerMiddleware
+    )
   )
 )
 
